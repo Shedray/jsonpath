3fcd4631a09622f90c62ad8ba58708cacd28e0fd
renpenglei <renpenglei@kuaishou.com>
2021-09-26 19:16:14 +0800

    tijiao

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default {
+    assetsPublicPath: '/',
     plugins: [
         // ...
         Components({
